fe2581e97399f4162090cd70abafaefc710d2470
angel <angelandresarmas@gmail.com>
2025-10-11 09:52:47 -0400

    fixed proposed changes in routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItem");
 
+// Mount resource routers
 router.use("/items", clothingItemRouter);
 router.use("/users", userRouter);
 
-router.use("*", (req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+// 404 handler (Express 5)
+router.all("*", (req, res) => {
+  res.status(404).json({ message: "Requested resource not found" });
 });
 
 module.exports = router;
